cc4a85642aba57eac5e22e26aeb23c6a214113f2
Jater <p833795p@gmail.com>
2022-03-06 16:56:58 +0800

    feat: import socket, 初步完成 查看線上使用者清單，其他使用者離開會即時更新清單，尚未把onlineCount 傳到 header。

---
+++ src/components/ChatRoom.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="chat-room-container">
+    <div class="chat-room-header-title">
+      <div class="header-title-page">{{ title }}</div>
+      <div class="header-title-tweet" v-show="isPrivateMessage">
+        {{ viewUser.tweetCount }} 推文
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      title: "公開聊天室"
+    }
+  }
+}
+</script>
